89be2ef4cdcd75f7073811bda5895b5e4900e76d
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-02 19:21:47 -0300

    mute reanimated warning

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import {LogBox} from 'react-native';
+
 import MainApp from './src';
+
+LogBox.ignoreLogs(['Reanimated 2']);
 
 const App = () => {
   return <MainApp />;
