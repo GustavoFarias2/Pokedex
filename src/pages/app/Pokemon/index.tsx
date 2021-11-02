45f75588126407d047a0769b5640ce19760717a3
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-02 15:08:54 -0300

    fetch pokemon when enter the pokemon page

--- src/pages/app/Pokemon/index.tsx
+++ src/pages/app/Pokemon/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react';
 
-// import { Container } from './styles';
+import {ActivityIndicator} from 'react-native-paper';
 
-const Pokemon: React.FC = () => {
-  return (
-    <View>
-      <Text>Pokemon</Text>
-    </View>
-  );
-};
+import {getPokemonApi} from '../../../services/api';
+
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../routes';
+import {PokemonType} from '../../../Types/types';
+
+const Pokemon: React.FC<NativeStackScreenProps<RootStackParamList, 'Pokemon'>> =
+  props => {
+    const {
+      route: {
+        params: {pokemonName},
+      },
+    } = props;
+
+    const [loading, setLoading] = useState(true);
+
+    const [pokemon, setPokemon] = useState<PokemonType | null>(null);
+
+    const getPokemon = useCallback(async () => {
+      const {data} = await getPokemonApi(pokemonName);
+
+      setPokemon(data);
+
+      setLoading(false);
+    }, [pokemonName]);
+
+    useEffect(() => {
+      getPokemon();
+    }, [getPokemon]);
+
+    if (loading || !pokemon) {
+      return (
+        <Container>
+          <ActivityIndicator animating />
+        </Container>
+      );
+    }
+
+    return (
+      <Container>
+        <Text>{pokemon.name}</Text>
+      </Container>
+    );
+  };
 
 export default Pokemon;
