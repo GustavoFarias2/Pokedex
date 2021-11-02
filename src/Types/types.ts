93033df9f216fd27b6858abd96efe3eafb374a24
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-02 17:29:18 -0300

    Add pokemon types

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -1,3 +1,36 @@
+import {pokemonApiResourceResponseItem} from '../services/api';
+
+type PokemonTypeType = {
+  slot: number;
+  type: pokemonApiResourceResponseItem;
+};
+
+type PokemonAbilityType = {
+  ability: pokemonApiResourceResponseItem;
+  is_hidden: boolean;
+  slot: number;
+};
+
+export type PokemonStatsType = {
+  base_stat: number;
+  effort: number;
+  stat: pokemonApiResourceResponseItem;
+};
+
 export type PokemonType = {
   name: string;
+  id: number;
+  sprites: {
+    other: {
+      home: {
+        front_default: string;
+      };
+    };
+  };
+  types: Array<PokemonTypeType>;
+  species: pokemonApiResourceResponseItem;
+  height: number;
+  abilities: Array<PokemonAbilityType>;
+  gender: 'Male' | 'Fem';
+  stats: Array<PokemonStatsType>;
 };
