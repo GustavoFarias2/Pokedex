e8d0540411bad5499d1bff23957ca2242a23dc22
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-03 19:44:20 -0300

    adjust redux

--- src/store/pokemons.store.ts
+++ src/store/pokemons.store.ts
@@ -1,22 +1,51 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {pokemonApiResourceResponseItem} from '../services/api';
+
+import {PokemonType} from './../Types/types';
+import {Filters} from './filters.store';
 
 const pokemons = createSlice({
   name: 'pokemons',
-  initialState: <pokemonApiResourceResponseItem[]>[],
+  initialState: {
+    pokemons: <PokemonType[]>[],
+    filteredPokemons: <PokemonType[]>[],
+  },
   reducers: {
-    setPokemons: (
-      _,
-      {payload}: PayloadAction<pokemonApiResourceResponseItem[]>,
-    ) => payload,
+    setPokemons: (_, {payload}: PayloadAction<PokemonType[]>) => ({
+      pokemons: payload,
+      filteredPokemons: payload,
+    }),
 
-    addPokemons: (
-      state,
-      {payload}: PayloadAction<pokemonApiResourceResponseItem[]>,
-    ) => [...state, ...payload],
+    addPokemons: (state, {payload}: PayloadAction<PokemonType[]>) => ({
+      pokemons: [...state.pokemons, ...payload],
+      filteredPokemons: [...state.filteredPokemons, ...payload],
+    }),
+
+    filterPokemons: (state, {payload}: PayloadAction<String[]>) => {
+      if (payload[0] === 'all' || payload.length === 0) {
+        return {
+          pokemons: state.pokemons,
+          filteredPokemons: state.pokemons,
+        };
+      }
+
+      const filteredPokemons = state.pokemons.filter(pokemon => {
+        const pokemonTypes = pokemon.types.map(type => type.type.name);
+
+        if (pokemonTypes.some(type => payload.includes(type))) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      return {
+        pokemons: state.pokemons,
+        filteredPokemons,
+      };
+    },
   },
 });
 
-export const {setPokemons, addPokemons} = pokemons.actions;
+export const {setPokemons, addPokemons, filterPokemons} = pokemons.actions;
 
 export default pokemons.reducer;
