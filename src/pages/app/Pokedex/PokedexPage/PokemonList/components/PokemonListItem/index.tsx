2d0eb3beb73c5f744f1311be456f9480e1982079
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-03 19:44:49 -0300

    now use all pokemon data

--- src/pages/app/Pokedex/PokedexPage/PokemonList/components/PokemonListItem/index.tsx
+++ src/pages/app/Pokedex/PokedexPage/PokemonList/components/PokemonListItem/index.tsx
@@ -1,8 +1,6 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React from 'react';
 
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-
-import {getPokemonApi} from '../../../../../../../services/api';
 
 import {useLinearGradient} from '../../../../../../../functions/useLinearGradient';
 
@@ -14,37 +12,23 @@ import {RootStackParamList} from '../../../../../../../routes';
 import {PokemonType} from '../../../../../../../types/types';
 
 interface ListRenderItemItemProps {
-  pokemon: {
-    name: string;
-  };
+  pokemon: PokemonType;
   navigation: NativeStackNavigationProp<RootStackParamList, any>;
 }
 
 const PokemonListItem: React.FC<ListRenderItemItemProps> = props => {
   const {pokemon, navigation} = props;
 
-  const [fetchedPokemon, setFetchedPokemon] = useState<PokemonType>();
-
-  const getPokemonImage = useCallback(async () => {
-    const {data} = await getPokemonApi(pokemon.name);
-
-    setFetchedPokemon(data);
-  }, [pokemon.name]);
-
-  useEffect(() => {
-    getPokemonImage();
-  }, [getPokemonImage]);
-
   const handleGoToPokemon = () =>
     navigation.navigate('Pokemon', {pokemonName: pokemon.name});
 
-  const colors = useLinearGradient(fetchedPokemon?.types);
+  const colors = useLinearGradient(pokemon.types);
 
   return (
     <TouchableWithoutFeedback onPress={handleGoToPokemon}>
       <Card colors={colors}>
         <PokemonImage
-          source={{uri: fetchedPokemon?.sprites.other.home.front_default}}
+          source={{uri: pokemon.sprites.other.home.front_default}}
         />
         <NameView>
           <Name>{pokemon.name}</Name>
