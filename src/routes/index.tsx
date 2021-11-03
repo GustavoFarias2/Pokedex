773ea7cbebe450359b171f6a8682d9b7d461ecfd
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-11-03 17:57:12 -0300

    add login route and login verification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,78 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {useDispatch, useSelector} from 'react-redux';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {loginUser} from '../store/auth.store';
+import {RootState} from '../store';
+
+import {ActivityIndicator} from 'react-native-paper';
+
+import {LoadingContainer} from './styles';
 
 import Pokedex from '../pages/app/Pokedex';
 import Pokemon from '../pages/app/Pokemon';
+import Login from '../pages/login/Login';
 
 export type RootStackParamList = {
   Pokedex: undefined;
   Pokemon: {
     pokemonName: string;
   };
+  Login: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const Routes: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const {token} = useSelector((state: RootState) => state.auth);
+
+  const [loading, setLoading] = useState(true);
+
+  const checkIfIsLogged = useCallback(async () => {
+    const tokenInStorage = await AsyncStorage.getItem('@pokedex_user_token');
+
+    if (tokenInStorage) {
+      dispatch(loginUser());
+    }
+
+    setLoading(false);
+  }, [dispatch]);
+
+  useEffect(() => {
+    checkIfIsLogged();
+  }, [checkIfIsLogged]);
+
+  if (loading) {
+    return (
+      <LoadingContainer>
+        <ActivityIndicator animating />
+      </LoadingContainer>
+    );
+  }
+
+  if (!token) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
